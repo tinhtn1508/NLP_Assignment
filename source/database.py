384bea526b64e448455571797d417a410d3fb2f4
Tuan Tran <TuanTranEngineer@github.com>
2020-08-21 22:03:58 +0700

    update logical form parsing

--- source/database.py
+++ source/database.py
@@ -1,9 +1,9 @@
 ruleTable = {
     "ROOT": { "den": "root", "di": "root", "xuat_phat": "root"},
-    "xuat_phat": {"nao": "which-query", "tu": "from-to"},
+    "xuat_phat": {"nao": "which-query", "tu": "from-loc"},
     "tu": {"thanh_pho": "to_loc", "<VAR-LOC>": "<var>name"},
     "den": { "nao": "which-query", "thanh_pho": "to-loc", "luc": "arrive-time", "<VAR-LOC>": "<var>name"},
-    "di": {"nao": "which-query", "den": "to-city", "tu": "from-to", "thoi_gian" : "time-query", "xe_bus": "lsubj"},
+    "di": {"nao": "which-query", "den": "to-city", "tu": "from-loc", "thoi_gian" : "time-query", "xe_bus": "lsubj"},
     "nao": { "xe_bus": "lsubj", "xe": "lsubj", "nhung": "plural"},
     "xe_bus": {"<VAR-BUS>": "<var>name"},
     "thanh_pho": { "<VAR-LOC>": "<var>name" },
--- source/logical_form_generator.py
+++ source/logical_form_generator.py
@@ -29,12 +29,12 @@ class QueryLogic:
 
     def getFromLoc(self):
         rootNode = self.tree.getRoot()
-        fromToNode = self.findEdgeNode(rootNode, "from-to")
+        fromToNode = self.findEdgeNode(rootNode, "from-loc")
         if fromToNode is None:
             return None
         nameNode = self.findEdgeNode(fromToNode, "<var>name")
         if nameNode is None:
-            raise Exception("<var>name not exist while from-to node is represent")
+            raise Exception("<var>name not exist while from-loc node is represent")
         return nameNode.nodeName()
 
     def getToLoc(self):
@@ -104,20 +104,79 @@ class QueryLogic:
 
 
 
-# class LogicalFormParser:
-#     def __init__(self, tree):
-#         if tree is None:
-#             raise Exception("input None tree!!!")
-#         if tree.getRoot() is None:
-#             raise Exception("input None root node!!!")
-#         # print("parse logical form for tree:\n{}".format(tree.printTree()))
-#         self.tree = tree
+class LogicalFormParser:
+    def __init__(self, tree):
+        if tree is None:
+            raise Exception("input None tree!!!")
+        if tree.getRoot() is None:
+            raise Exception("input None root node!!!")
+        self.varCount = {}
+        self.tree = tree
 
-#     def queryLogic(self):
+    def findEdgeNode(self, node, edgeName):
+        if node is None or node.numChildren() == 0: None
+        for cnode in node.childrenIter():
+            if cnode.edgeType == edgeName:
+                return cnode
+            lowerResult = self.findEdgeNode(cnode, edgeName)
+            if lowerResult is not None: return lowerResult
+        return None
 
+    def chooseVarName(self, varType):
+        c = varType[0].lower()
+        if c < 'a' or c > 'z': c = 'n'
+        if c in self.varCount:
+            self.varCount[c] += 1
+        else:
+            self.varCount[c] = 1
+        return c + str(self.varCount[c])
 
+    def parseAllChild(self, node):
+        if node is None:
+            raise Exception("input node is None!!!")
+        describe = []
+        for cnode in node.childrenIter():
+            nodeType = cnode.nodeType()
+            nodeName = cnode.nodeName()
+            if "<var>" == nodeType[0:5]:
+                obj = nodeType[5:].upper()
+                nodeName = nodeName.upper()
+                varName = self.chooseVarName(nodeName)
+                describe.append("<{} {} {}>".format(obj, varName, nodeName))
+            elif "lsubj" == nodeType:
+                describe.append("({} {})".format(nodeName.upper(), self.chooseVarName(nodeName)))
+            elif "plural" == nodeType:
+                describe.insert(0, "PLURAL")
+            else:
+                describe.append(cnode.nodeName().upper())
 
-    # def parse(self):
+            describe.append(self.parseAllChild(cnode))
+
+        if len(describe) == 0: return ""
+        return "".join(describe)
+
+    def parseRootBranch(self, node, var):
+        if node is None:
+            raise Exception("input node is None!!!")
+        nodeType = node.nodeType().upper()
+        return "({} {} {})".format(nodeType, var, self.parseAllChild(node))
+
+    def parse(self):
+        varCount = { 'e': 1 }
+        describe = []
+        lform = "(∃ e1: (&\n{}\n))"
+
+        rootNode = self.tree.getRoot()
+        mainVerbNode = self.findEdgeNode(rootNode, "root")
+        if mainVerbNode is None:
+            raise Exception("There is no main verb in this tree!\n{}".format(self.tree.printTree()))
+        mainVerb = mainVerbNode.nodeName().upper()
+        describe.append("({} e1)".format(mainVerb))
+
+        for cnode in mainVerbNode.childrenIter():
+            describe.append(self.parseRootBranch(cnode, "e1"))
+
+        return lform.format("    " + ("\n    ").join(describe))
 
 
 
@@ -141,6 +200,8 @@ for question in questions:
     # tree.printTree()
     query = QueryLogic(tree)
     query.parse()
+    logical = LogicalFormParser(tree)
+    print(logical.parse())
     print("Question: {}".format(question))
     print("Answer: {}".format(query.answer()))
 
